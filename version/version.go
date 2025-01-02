9a7625d250ee4c76f1c0d6a934850c5444c92149
Bryan Borgeson <bryborge@gmail.com>
2025-01-02 20:41:46 +0000

    asdf

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 import "github.com/hashicorp/packer-plugin-sdk/version"
 
 var (
-	Version           = "0.1.9"
+	Version           = "0.2.1"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 	PluginVersion     = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
